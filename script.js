1a54c1968749b223edc5d5672a6b4266a59de8d9
Promtech <uwagboepromise5@gmail.com>
2023-06-08 16:50:52 +0100

    Added functionality

---
+++ script.js
@@ -0,0 +1,52 @@
+let adviceId = document.querySelector('.advice-id')
+let advice = document.querySelector('.advice')
+let dice = document.querySelector('.dice-main')
+let diceError = document.querySelector('.error')
+
+async function getAdvice(){
+    try {
+        let timestamp = Date.now(); // Get current timestamp
+        let url = `https://api.adviceslip.com/advice?timestamp=${timestamp}`; // Append timestamp as a query parameter
+        let response = await fetch(url);
+        let data = await response.json();
+
+        let dataAdvice = data.slip.advice;
+        let dataId = data.slip.id;
+        adviceId.innerHTML = `Advice #${dataId}`;
+        advice.innerHTML = `<p>${dataAdvice}</p>`;
+        console.log(dataId);
+        console.log(dataAdvice);
+    } catch (error) {
+        diceError.innerHTML = 'Please check your internet connection'
+    }
+}
+
+getAdvice();
+
+
+// Below Is the second alternative
+// let adviceId = document.querySelector('.advice-id')
+// let advice = document.querySelector('.advice')
+// let dice = document.querySelector('.dice-main')
+
+// async function getAdvice(){
+    
+//     try {
+//         let response = await fetch('https://api.adviceslip.com/advice', {
+//         cache: 'no-store', // Set cache control to "no-store" to disable caching
+//         });
+//         let data = await response.json();
+
+//         let dataAdvice = data.slip.advice;
+//         let dataId = data.slip.id;
+//         adviceId.innerHTML = `Advice ${dataId}`;
+//         advice.innerHTML = `<p>${dataAdvice}</p>`;
+//         console.log(dataId);
+//         console.log(dataAdvice);
+//     } catch (error) {
+//         console.log('PLEASE CHECK YOUR INTERNET CONNECTION');
+//     }
+    
+// }
+
+// getAdvice();
